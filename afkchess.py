6e5b5172570b2ac8881beb6fb2f662ad6c066f5e
oliebolie <151738254+oliebolie@users.noreply.github.com>
2024-03-26 20:12:37 +0100

    Add files via upload

---
+++ afkchess.py
@@ -0,0 +1,33 @@
+from pygame import mixer
+import time
+import pyautogui
+import keyboard
+
+mixer.init()
+plays = mixer.Sound("beep.wav")
+
+def repeat_until_stop():
+    while True:
+        notmyturn = pyautogui.pixelMatchesColor(112, 494, (152, 151, 149))
+        if notmyturn:
+            print("It's not your turn yet.")
+            while notmyturn:
+                notmyturn = pyautogui.pixelMatchesColor(112, 494, (152, 151, 149))
+                time.sleep(1)
+            alarm()
+            print("It's your turn! Do you want to run again? (y/n)")
+            choice = input().lower()
+            if choice != 'y':
+                break
+        else:
+            print("It's your turn!")
+            alarm()
+
+def alarm():
+    plays.play()
+    time.sleep(3)
+    plays.play()
+
+repeat_until_stop()
+
+
